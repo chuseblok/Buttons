2760690bcb06d5bcd983a974fdb473795cb2f436
sebastian Chunley <sebastian.chunley@gmail.com>
2022-07-27 09:18:02 -0400

    updating

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <div class='Watermelon'>
-          <div class='font'></div>
-          <div class='inside'></div>
-          <div class='style'>
-            <div></div>
-            <div></div>
+          <div class='slice'>
+            <div class='inside'></div>
+            <div class='style'>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+          <div class='whole'>
             <div></div>
             <div></div>
             <div></div>
